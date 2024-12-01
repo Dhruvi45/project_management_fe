b2f5493be975a81f42f23733764312393a2d4ef8
Dhruvi45 <78956485+Dhruvi45@users.noreply.github.com>
2024-12-01 12:38:30 +0000

    test ci cd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,26 @@
 export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-gray-400 py-3">
+  return (
+    <footer className="bg-gray-800 text-gray-400 py-3">
       <div className="container mx-auto text-center space-y-2">
         <p>
-          <a href="https://github.com/Dhruvi45/" target="_blank" className="hover:underline">
+          <a
+            href="https://github.com/Dhruvi45/"
+            target="_blank"
+            className="hover:underline"
+          >
             GitHub
           </a>{" "}
           |{" "}
-          <a href="https://www.linkedin.com/in/dhruvi-joshi-467160177/" target="_blank" className="hover:underline">
+          <a
+            href="https://www.linkedin.com/in/dhruvi-joshi-467160177/"
+            target="_blank"
+            className="hover:underline"
+          >
             LinkedIn
           </a>
         </p>
         <p>Dhruvi Joshi</p>
       </div>
     </footer>
-    );
-  }
-  
+  );
+}
